57a7bc97652d2efc38002f7d0a184e31f4bd16e1
puriMouryan25 <parjun000000@gmail.com>
2025-04-03 00:45:51 -0700

    file modified to link well with run pipeline

--- src/evaluate_quality.py
+++ src/evaluate_quality.py
@@ -2,12 +2,12 @@ from evaluate import compute_fid
 
 # -------- CONFIG --------
 real_dir = "../dermDatabaseOfficial/release_v0/images"
-synthetic_dir = "./synthetic_dataset"
+synthetic_dir = "../synthetic_dataset"
 # ------------------------
 
 print("\n🔍 Evaluating FID and SSIM between real and synthetic images...\n")
 
 # FID
 fid_score = compute_fid(real_dir, synthetic_dir)
-print(f"📊 FID Score: {fid_score:.4f}  (lower is better)")
+print(f"📊 FID Score: {fid_score:.4f} ")
 
